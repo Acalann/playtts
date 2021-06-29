ab1793d97cbd941a7c5d5977619902fe3db21307
itsmii <64848274+itsmii@users.noreply.github.com>
2021-06-29 21:51:02 +0100

    Update playtts.py

--- playtts.py
+++ playtts.py
@@ -1,8 +1,19 @@
 from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
+from os import remove
 
 
-def playtts(whatosay, filetosaveto):
-    while True:
-        tts = gTTS(whatosay)
-        tts.save(filetosaveto + '.mp3')
-        quit()
+def savetts(whatosay, filetosaveto):
+    tts = gTTS(whatosay)
+    tts.save(filetosaveto + '.mp3')
+    quit()
+
+
+def playtts(whatosay):
+    tts = gTTS(whatosay)
+    tts.save('tmp.mp3')
+    sound = AudioSegment.from_mp3('tmp.mp3')
+    play(sound)
+    remove('tmp.mp3')
+    quit()
